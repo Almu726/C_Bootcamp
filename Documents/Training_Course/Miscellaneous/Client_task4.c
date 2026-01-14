73bd84471cdf42ee1cc7625e2d9afeaaf2fde7a0
almu726 <almubarak.idris@gmail.com>
2026-01-14 14:09:13 +0000

    Task 4 Communications

--- Documents/Training_Course/Miscellaneous/Client_task4.c
+++ Documents/Training_Course/Miscellaneous/Client_task4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>     // for strlen()
-#include <unistd.h>     // for close()
+#include <string.h>     
+#include <unistd.h>    
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -8,4 +8,48 @@
 int main() {
 
     int sd = socket(AF_INET, SOCK_STREAM, 0);
+
+
+    if (sd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(5000);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        return 1;
+    }
+
+    if (connect(sd, (struct sockaddr*)&addr, sizeof(addr)) < 0){
+        perror("connect");
+        return 1;
+    }
+
+
+
+    char buffer[1024];
+
+    while (1) {
+
+        int bytes = recv(sd, buffer, sizeof(buffer)-1, 0);
+
+
+        if (bytes <= 0) {
+            perror("recv");
+            break;
+        }
+
+        buffer[bytes] = '\0';  
+
+        printf("Current time from server: %s\n", buffer);
+
+    }
+
+    close(sd);
+    return 0;
 }
---
+++ Documents/Training_Course/Miscellaneous/Server_task4.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>    
+#include <unistd.h>    
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+int main(){
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(5000);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(sd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    listen(sd,1);
+
+    int client_sd = accept(sd, NULL, NULL);
+
+    char buffer[1024];
+    int bytes = recv(client_sd, buffer, sizeof(buffer), 0);
+
+
+
+    while (1) {
+
+        time_t now = time(NULL);          
+        struct tm *t = localtime(&now);  
+
+        char date_str[128];
+        strftime(date_str, sizeof(date_str), "%Y-%m-%d %H:%M:%S", t);
+        // format time into a readable string
+
+        if (send(client_sd, date_str, strlen(date_str), 0) < 0) {
+            perror("send");
+            break;
+        }
+
+        printf("Sent time to client: %s\n", date_str);
+
+        sleep(5); 
+    }
+
+    close(client_sd);
+    close(sd);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
